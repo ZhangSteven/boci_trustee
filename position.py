76b4fd0a049a2ee51a07e572ea2bd1de923c9fbf
Steven Zhang <steven.zhang@CLAMC.COM.HK>
2021-01-15 18:22:24 +0800

    working on position.py

---
+++ position.py
@@ -0,0 +1,160 @@
+# coding=utf-8
+# 
+# Extract cash and holding positions from BOCI Trustee valuation 
+# report to Geneva reconciliation files.
+# 
+
+from toolz.functoolz import compose
+from steven_utils.utility import writeCsv
+from stbf.valuation_report import getValuationDataFromFile
+from functools import partial
+from itertools import chain
+import logging
+logger = logging.getLogger(__name__)
+
+
+
+"""
+1. read valuation report from boci trustee, get data;
+
+2. convert cash data to cash recon file;
+
+3. combine fixed deposit, bond data and convert to position recon file;
+
+4. return cash file, position file.
+
+"""
+
+def processValuationFile(file):
+	"""
+	[String] file 
+		=> [Tuple] ( [String] cash recon file
+				   , [String] position recon file
+				   )
+	"""
+	logger.debug('processValuationFile(): {0}'.format(file))
+
+	date, _, _, bondPositions, cashPositions = \
+		getValuationDataFromFile(file)
+
+	return ( createCashReconFile(getParentFolder(file), date, cashPositions)
+		   , createPositionReconFile(getParentFolder(file), date, bondPositions))
+
+
+
+getPortfolioName = lambda : 'Short Term Bond Fund'
+
+
+getCashReconFields = lambda : \
+	('portfolio', 'custodian', 'date', 'currency', 'balance')
+
+
+getPositionReconFields = lambda : \
+	( 'portfolio', 'custodian', 'date', 'geneva_investment_id'
+	, 'ISIN', 'bloomberg_figi', 'name', 'currency', 'quantity'
+	)
+
+
+dictToValues = lambda keys, d: \
+	map(lambda key: d[key], keys)
+
+
+
+"""
+	[String] date,
+	[Dictionary] cash position
+		=> [Dictionary] cash reconciliation position
+"""
+cashReconPosition = lambda date, position: \
+	{ 'portfolio': getPortfolioName()
+	, 'custodian': ''
+	, 'date': date
+	, 'currency': position['DEAL CCY']
+	, 'balance': position['ORIG CURR BOOK COST']
+	}
+				
+
+
+"""
+	[String] date,
+	[Dictionary] fixed deposit position
+		=> [Dictionary] holding reconciliation position
+"""
+# fixedDepositReconPosition = lambda date, fd: \
+# 	{ 'portfolio': getPortfolioName()
+# 	, 'custodian': ''
+# 	, 'date': date
+# 	, 'geneva_investment_id': getFixedDepositInvestId(fd)
+# 	, 'ISIN': ''
+# 	, 'bloomberg_figi': ''
+# 	, 'name': fd['INVESTMENT']
+# 	, 'currency': fd['DEAL CCY']
+# 	, 'quantity': fd['ORIG CURR BOOK COST']
+# 	}
+
+
+
+"""
+	[String] date,
+	[Dictionary] bond position
+		=> [Dictionary] holding reconciliation position
+"""
+bondReconPosition = lambda date, bond: \
+	{ 'portfolio': getPortfolioName()
+	, 'custodian': ''
+	, 'date': date
+	, 'geneva_investment_id': ''
+	, 'ISIN': bond['ISIN CODE']
+	, 'bloomberg_figi': ''
+	, 'name': bond['INVESTMENT']
+	, 'currency': bond['DEAL CCY']
+	, 'quantity': bond['NOMINAL QUANTITY']
+	}
+
+
+# [String] file name => [String] suffix 
+getParentFolder = compose(
+	lambda L: '_'.join(L)
+  , lambda s: s.split()
+  , lambda file: file.split('\\')[-2]
+)
+	
+
+
+def createCashReconFile(prefix, date, cashPositions):
+	"""
+	[String] prefix, [String] date, [Iterable] cash positions
+		=> [String] cash reconciliation file name
+
+	Side effect: write a csv file
+	"""
+	return \
+	compose(
+		lambda values: \
+			writeCsv( prefix + '_cash_' + date + '.csv'
+					, chain([getCashReconFields()], values)
+					, delimiter='|'
+					)
+	  , partial(map, partial(dictToValues, getCashReconFields()))
+	  , partial(map, partial(cashReconPosition, date))
+	)(cashPositions)
+
+
+
+def createPositionReconFile(prefix, date, bondPositions):
+	"""
+	[String] prefix, [String] date, [Iterable] bond positions
+		=> [String] holding reconciliation file name
+
+	Side effect: write a csv file
+	"""
+	return \
+	compose(
+		lambda values: \
+			writeCsv( prefix + '_position_' + date + '.csv'
+					, chain([getPositionReconFields()], values)
+					, delimiter='|'
+					)
+	  , partial(map, partial(dictToValues, getPositionReconFields()))
+	  , partial(map, partial(bondReconPosition, date))
+	)(bondPositions)
---
+++ test/test_position.py
@@ -0,0 +1,55 @@
+# coding=utf-8
+# 
+
+import unittest2
+from boci_trustee.utility import getCurrentDir
+from boci_trustee.position import processValuationFile
+from os.path import join
+
+
+
+class TestPosition(unittest2.TestCase):
+
+	def __init__(self, *args, **kwargs):
+		super(TestPosition, self).__init__(*args, **kwargs)
+
+
+
+	def testProcessValuationFile(self):
+		inputFile = join(getCurrentDir(), 'samples', 'sample 2021-01-12.xls')
+		processValuationFile(inputFile)
+
+
+
+	# def testCashPosition(self):
+	# 	inputFile = join(getCurrentDir(), 'samples', 'sample 2021-01-12.xls')
+	# 	trades, _ = getBociTrades(fileToLines(inputFile))
+	# 	t = list(trades)[1]
+	# 	self.assertEqual('12345678', t['Account'])
+	# 	self.assertEqual('BK5JS96', t['SEDOL'])
+	# 	self.assertEqual('XS1813551584', t['ISIN'])
+	# 	self.assertEqual('HOPSON DEVELOP', t['Name'])
+	# 	self.assertEqual('S', t['TranType'])
+	# 	self.assertEqual(1996000, t['Quantity'])
+	# 	self.assertEqual('07/10/2020', t['TradeDate'])
+	# 	self.assertEqual('09/10/2020', t['SettlementDate'])
+	# 	self.assertEqual('USD', t['Currency'])
+	# 	self.assertEqual(100.58, t['Price'])
+	# 	self.assertEqual(42415, t['AccurredInterest'])
+	# 	self.assertEqual(2049991.80, t['SettlementAmount'])
+	# 	self.assertEqual(0, t['Commission'])
+	# 	self.assertEqual('', t['StampDuty'])
+	# 	self.assertEqual('', t['TransactionLevy'])
+	# 	self.assertEqual('', t['ClearingFee'])
+	# 	self.assertEqual('', t['SalesTax'])
+	# 	self.assertEqual('', t['HongKongCCASSFee'])
+	# 	self.assertEqual('281305', t['TradeReferenceNumber'])
+	# 	self.assertEqual('94589', t['BrokerCode'])
+	# 	self.assertEqual('GOLDMAN SACHS', t['BrokerName'])
+
+
+
+	# def testTrade2(self):
+	# 	inputFile = join(getCurrentDir(), 'samples', 'sample_trade2.xlsx')
+	# 	_, tradesWithMultipleSSI = getBociTrades(fileToLines(inputFile))
+	# 	self.assertEqual(['281305', '282617', '283003'], tradesWithMultipleSSI)
